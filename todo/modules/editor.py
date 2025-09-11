daf0548722b267feb3954417b3e7d04054da3c0f
Fabian <fsteube@ep1.rub.de>
2025-09-11 21:23:22 +0200

    Remove dictionary.

--- todo/modules/editor.py
+++ todo/modules/editor.py
@@ -5,12 +5,6 @@ import os
 from .models import ToDoManager, Matter, Task
 from .utils import list_content, write_content_txt, write_content_json
 
-header_dict: dict[int, str] = {
-    1: "===| Fragen/Vorschlaege |===",
-    2: "===| Tasks |===",
-    3: "===| Done Tasks |==="
-}
-
 class ToDoEditor:
     def __init__(self) -> None:
         self.file: str = "todo.json"
@@ -120,7 +114,7 @@ class ToDoEditor:
             print(f"File {file} exists already")
             return
         
-        lines = [header_dict[1], "\n", header_dict[2], "\n", header_dict[3]]
+        lines = ["===| Fragen/Vorschlaege |===", "\n", "===| Tasks |===", "\n", "===| Done Tasks |==="]
 
         write_content_txt(file, lines)
         print(f"Created empty txt file named {file}")
@@ -130,9 +124,9 @@ class ToDoEditor:
         pass
 
     def export_to_txt(self, file: str):
-        matter_list: list[str] = list_content(header_dict[1], self.manager.matters)
-        task_list: list[str] = list_content(header_dict[2], self.manager.tasks)
-        done_list: list[str] = list_content(header_dict[3], self.manager.done)
+        matter_list: list[str] = list_content("===| Fragen/Vorschlaege |===", self.manager.matters)
+        task_list: list[str] = list_content("===| Tasks |===", self.manager.tasks)
+        done_list: list[str] = list_content("===| Done Tasks |===", self.manager.done)
 
         content_list = matter_list + task_list + done_list
 
